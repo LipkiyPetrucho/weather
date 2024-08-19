d72e64107f7127a0f8f52fa14ff3cf04cb05d3fa
Petr <bang1987@mail.ru>
2024-08-19 10:38:04 +0400

    added network name to docker-compose.yml and a command to grant init.sh file initialisation permissions

--- weather_project/init.sh
+++ weather_project/init.sh
@@ -2,5 +2,6 @@
 set -e
 
 #python ./manage.py collectstatic --noinput
-python ./manage.py migrate
+python ./manage.py makemigrations --noinput
+python ./manage.py migrate --noinput
 poetry run gunicorn --bind 0.0.0.0:8000 weather_project.wsgi:application --timeout 120
