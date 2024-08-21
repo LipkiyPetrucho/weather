a630da72dcb1d911a079afde5d1bde75fc660a02
Petr <bang1987@mail.ru>
2024-08-21 23:57:46 +0400

    removed unnecessary gunicorn service from docker-compose corrected commands in init.sh and resaved in LF(UNIX) format

--- weather_project/init.sh
+++ weather_project/init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-set -e
-
-#python ./manage.py collectstatic --noinput
-python ./manage.py makemigrations --noinput
-python ./manage.py migrate --noinput
+python ./manage.py migrate
 poetry run gunicorn --bind 0.0.0.0:8000 weather_project.wsgi:application --timeout 120
